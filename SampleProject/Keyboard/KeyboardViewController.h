4742a88c07b49e8e246ec5e660c7df61ab434694
Adam Wolf <adamawolf@gmail.com>
2014-12-18 13:37:16 -0800

    integrate BaseBoard as a cocoapod

---
+++ SampleProject/Keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  BaseBoard
+//
+//  Created by Adam A. Wolf on 12/18/14.
+//  Copyright (c) 2014 Adam A. Wolf. All rights reserved.
+//
+
+#import "BBDKeyboardViewController.h"
+
+@interface KeyboardViewController : BBDKeyboardViewController
+
+@end
---
+++ SampleProject/Keyboard/KeyboardViewController.m
@@ -0,0 +1,17 @@
+//
+//  KeyboardViewController.m
+//  BaseBoard
+//
+//  Created by Adam A. Wolf on 12/18/14.
+//  Copyright (c) 2014 Adam A. Wolf. All rights reserved.
+//
+
+#import "KeyboardViewController.h"
+
+@interface KeyboardViewController ()
+
+@end
+
+@implementation KeyboardViewController
+
+@end
